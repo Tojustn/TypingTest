41bd27b947dceec41598c1c6aed2e93e0514de8d
Tojustn <Tojustn@gmail.com>
2025-02-14 14:24:29 -0600

    Initial Stage

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+//Declare ES module for import 
+const app = express();
+
+//Accept JSON
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// Starts server on port and callback
+app.listen(PORT, ()=>{
+    console.log(`App run on http://localhost:${PORT}/`)
+})
